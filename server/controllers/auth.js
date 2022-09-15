ec31ab4fd1877c0d2c3b7168368741c7b2cb7ba9
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-09-15 23:45:20 +0600

    jwt token generate

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const { Validation } = require("../helpers/Validaton");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.userLogin = async (req, res) => {
   res.json({ Message: "This is user login" });
@@ -20,7 +21,10 @@ exports.userRegister = async (req, res) => {
     req.body.password = await bcrypt.hash(password, 10);
     const user = new User(req.body);
     await user.save();
-    res.status(200).json(user);
+    const accessToken = jwt.sign({ name: user.name, email: user.email, _id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "30 days",
+    });
+    res.status(200).json({ user, accessToken });
   } catch (err) {
     console.log(err);
   }
